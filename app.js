88b64906ebde3f533e9e914e0232c4a8a14bb525
Taha Qamar <tahakamar97@gmail.com>
2024-02-22 03:15:41 +0500

    add two file

---
+++ app.js
@@ -0,0 +1,56 @@
+// There are three ways to get output in browser
+// alert(typeof 10)
+// document.write(10)
+// console.log(100)
+ 
+// var a = 100
+// console.log(a)
+// var firstname = "Taha"
+// var lastname = "Qamar"
+// console.log(firstname + lastname)
+// console.log(firstname)
+// console.log(lastname)
+
+// Chapter no 01
+ 
+// Q1
+// alert("Error! Please enter a valid password")
+// Q2
+// alert("\n Welcome to JS land... \nHappy coding!")
+// Q3
+// alert("\n Welcome to JS land...")
+// Q4
+// alert("happy Coding! \n  Prevent this page from creating additional dialogs" )
+// Q5
+// alert("\n Hello... I can run JS through my web brower's console")
+
+// Chapter no 02
+// Q1
+// var username = "Taha Qamar";
+// alert(username)
+// Q2
+// var age = 15
+// alert( age + " years old")
+// Q3
+// var bio = "Certified Mobile Appliction development"
+// alert(bio)
+// Q5
+// var w1 = "PIZZA"
+// alert("\n"+  w1 + " \nPIZZ \nPIZ \nPI \nP " )
+// Q6
+// var email = "example@example.com";
+// alert("My email adress is "+ email )
+// Q7
+// var book = "I am trying to learn from the Book"
+// alert(book + " A smarter\nway to learn JavaScript ") 
+// Q8
+// document.write("Yah! A can wirte HTML content through javaScript")
+// Q9
+// var store = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert("\n" + store)
+// Chapter no 03
+// Q3
+// var number = 1990;
+// document.write("My birth year is "+ number+ "<br>Data type of my declared variable is number ")
+// Q4
+document.write("<b>John Doe</b> ordered <b> 5 T-shirt</b>(s) on XYZ Clothing store")
